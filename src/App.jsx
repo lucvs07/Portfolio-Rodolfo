8101bd57229dda7fc27ed88172a2e20c64e31e1d
Lucas Rodrigues Grecco <lucas.rgrecco@gmail.com>
2025-02-21 11:19:57 -0300

    simpliicando as classses

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 
 function App() {
   const hover = 'transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-102'
-
+  const bgImg = 'bg-[url(./assets/bg.svg)] bg-cover bg-center'
   return (
     <>
       <header>
-        <div className='w-full h-64 bg-[url(./assets/bg.svg)] bg-cover bg-center'>
+        <div className={`w-full h-64 ${bgImg}`}>
         </div>
       </header>
       <main>
@@ -20,17 +20,17 @@ function App() {
           </div>
         </section>
         <section className='grid grid-cols-3 grid-rows-9 gap-4 m-8'>
-          <div className={`row-span-2 w-full h-full bg-[url(./assets/bg.svg)] bg-cover bg-center rounded-md ${hover}`}>
+          <div className={`row-span-2 w-full h-full ${bgImg} ${hover} rounded-md`}>
           </div>
-          <div className={`w-full h-64 bg-[url(./assets/bg.svg)] bg-cover bg-center rounded-md ${hover}`}>
+          <div className={`w-full h-64 ${bgImg} ${hover} rounded-md`}>
           </div>
-          <div className={`w-full h-64 bg-[url(./assets/bg.svg)] bg-cover bg-center rounded-md ${hover}`}>
+          <div className={`w-full h-64 ${bgImg} ${hover} rounded-md`}>
           </div>
-          <div className={`row-span-2 w-full h-full bg-[url(./assets/bg.svg)] bg-cover bg-center rounded-md ${hover}`}>
+          <div className={`row-span-2 w-full h-full ${bgImg} ${hover} rounded-md`}>
           </div>
-          <div className={`row-span-2 w-full h-full bg-[url(./assets/bg.svg)] bg-cover bg-center rounded-md ${hover}`}>
+          <div className={`row-span-2 w-full h-full ${bgImg} ${hover} rounded-md`}>
           </div>
-          <div className={`w-full h-64 bg-[url(./assets/bg.svg)] bg-cover bg-center rounded-md ${hover}`}>
+          <div className={`w-full h-64 ${bgImg} ${hover} rounded-md`}>
           </div>
         </section>
         
